8a5032e7e5a869bca69b25c873440c5a9c8308c5
vinit <vinit.devani@codemonk.ai>
2022-07-23 11:06:11 +0530

    Remove unncesary file

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 function Home() {
   const navigate = useNavigate();
@@ -18,23 +19,30 @@ function Home() {
   };
 
   return (
-    <div className="App-header">
-      {posts.length > 0 &&
-        posts.map((ele, i) => {
-          return (
-            <div
-              key={i}
-              className="card"
-              onClick={() => handleUserDetails(ele?.id)}
-            >
-              <h4 className="mb0">Title:</h4>
-              <p>{ele?.title}</p>
-              <h4 className="mb0">Description: </h4>
-              <p>{ele?.body}</p>
-            </div>
-          );
-        })}
-    </div>
+    <>
+      <Helmet>
+        <title>Home</title>
+        <meta name="title" content={"Home page of testing"} />
+        <meta name="description" content={"Home page of description"} />
+      </Helmet>
+      <div className="App-header">
+        {posts.length > 0 &&
+          posts.map((ele, i) => {
+            return (
+              <div
+                key={i}
+                className="card"
+                onClick={() => handleUserDetails(ele?.id)}
+              >
+                <h4 className="mb0">Title:</h4>
+                <p>{ele?.title}</p>
+                <h4 className="mb0">Description: </h4>
+                <p>{ele?.body}</p>
+              </div>
+            );
+          })}
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,33 +3,16 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { hydrateRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(
-    rootElement,
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  );
-} else {
-  hydrateRoot(
-    rootElement,
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-}
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </>
-// );
+  </>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
